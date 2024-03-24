4b522e185d9957a53029d9fcf48fd72f57a4856c
renantrendt <159676672+renantrendt@users.noreply.github.com>
2024-03-24 12:38:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // script.js
 
+// Função para salvar o conteúdo da textarea no LocalStorage
+function salvarNotaAutomaticamente() {
+    const nota = document.getElementById('notepad').value;
+    localStorage.setItem('minhaNota', nota);
+}
+
+// Quando o conteúdo da textarea muda, salva automaticamente
+document.getElementById('notepad').addEventListener('input', salvarNotaAutomaticamente);
+
+// Quando a página carrega, carrega a nota salva e coloca na textarea
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Quando a página carregar, verifique se já existe alguma nota salva
     const notaSalva = localStorage.getItem('minhaNota');
     if (notaSalva) {
         document.getElementById('notepad').value = notaSalva;
     }
 });
-
-function salvarNota() {
-    // Salvar a nota no armazenamento local quando o botão é clicado
-    const nota = document.getElementById('notepad').value;
-    localStorage.setItem('minhaNota', nota);
-}
